e6b76dc52c77cf2d53a44e0224814009c0b9c3e0
Thom-D02 <68430741+Thom-D02@users.noreply.github.com>
2023-08-27 12:54:26 +0100

    Add files via upload

---
+++ Testing.py
@@ -0,0 +1,33 @@
+from . import data_generation as generate
+from . import mean_grouping as group
+from Data_Display import Scatter
+from random import randint
+import turtle
+
+scatterTurtle = turtle.Turtle()
+
+data = generate.generate_uncorrelated(100, [-100,-100], [100,100],2)
+data = group.split(data, 5)
+scatterTurtle.color("blue")
+Scatter.draw_group(data[0], scatterTurtle)
+scatterTurtle.color("yellow")
+Scatter.draw_group(data[1], scatterTurtle)
+scatterTurtle.color("red")
+Scatter.draw_group(data[2], scatterTurtle)
+scatterTurtle.color("Orange")
+Scatter.draw_group(data[3], scatterTurtle)
+scatterTurtle.color("purple")
+Scatter.draw_group(data[4], scatterTurtle)
+input()
+scatterTurtle.clear()
+for i in range(20):
+    averages = group.find_averages(data,2)
+    data = group.reassign(data, averages)
+    data = group.remove_groups(data)
+scatterTurtle.color("black")
+Scatter.draw_group(averages, scatterTurtle)
+turtle.colormode(255)
+for group in data:
+    scatterTurtle.color(randint(0,255),randint(0,255),randint(0,255))
+    Scatter.draw_group(averages, scatterTurtle)
+input()
